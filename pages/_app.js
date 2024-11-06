e117955bf17157e03e255c6c58b00a068a7a6026
Andreas Kopp <andreas.kopp90@gmail.com>
2024-11-06 08:59:52 +0100

    fixed merge conflict

--- pages/_app.js
+++ pages/_app.js
@@ -14,31 +14,30 @@ export default function App({ Component, pageProps }) {
     setActivities(activities.filter((activity) => activity.id !== id));
     alert("deleted successfully");
     router.push("/");
-  function handleEditActivity(newActivity) {
-    if (activities.find((activity) => activity.id === newActivity.id)) {
-      setActivities(
-        activities.map((activity) => {
-          if (activity.id === newActivity.id) {
-            return newActivity;
-          }
-          return activity;
-        })
-      );
-      return;
+    function handleEditActivity(newActivity) {
+      if (activities.find((activity) => activity.id === newActivity.id)) {
+        setActivities(
+          activities.map((activity) => {
+            if (activity.id === newActivity.id) {
+              return newActivity;
+            }
+            return activity;
+          })
+        );
+        return;
+      }
     }
-    
-
+    return (
+      <>
+        <GlobalStyle />
+        <Component
+          handleAddActivity={handleAddActivity}
+          handleEditActivity={handleEditActivity}
+          handleDeleteActivity={handleDeleteActivity}
+          activities={activities}
+          {...pageProps}
+        />
+      </>
+    );
   }
-  return (
-    <>
-      <GlobalStyle />
-      <Component
-        handleAddActivity={handleAddActivity}
-        handleEditActivity={handleEditActivity}
-        handleDeleteActivity={handleDeleteActivity}
-        activities={activities}
-        {...pageProps}
-      />
-    </>
-  );
 }
