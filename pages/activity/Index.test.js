cd23d82ed2337ce115f8436cb68c89167b79e700
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-11-19 16:16:50 +0100

    write test filter remains open

--- pages/activity/Index.test.js
+++ pages/activity/Index.test.js
@@ -13,7 +13,7 @@ describe("2.1 The widget should be hidden by default on page load", () => {
   });
 });
 
-describe("2.2 Clicking the toggle button opens the filter widget", () => {
+describe("2.2 Clicking the toggle button opens/hides the filter widget", () => {
   test("Filter toggle", async () => {
     const user = userEvent.setup();
     render(<HomePage activities={activityData} filter={categories} />);
@@ -21,8 +21,34 @@ describe("2.2 Clicking the toggle button opens the filter widget", () => {
     const button = screen.getByRole("button", { name: /Filter/i });
     expect(button).toBeInTheDocument();
 
-    await user.click(button);
+    await user.click(button); // Show filter widget
+    expect(screen.getByTestId("filter")).toBeInTheDocument();
 
+    await user.click(button); // Hide filter widget
+    expect(screen.queryByTestId("filter")).not.toBeInTheDocument();
+  });
+});
+
+describe("2.3 The widget remains open if filters are applied", () => {
+  test("Filter remains open", async () => {
+    const user = userEvent.setup();
+    render(
+      <HomePage
+        activities={activityData}
+        filter={categories}
+        handleFilter={() => {}}
+      />
+    );
+
+    const button = screen.getByRole("button", { name: /Filter/i });
+    expect(button).toBeInTheDocument();
+
+    await user.click(button); // Show filter widget
+
+    const checkbox = screen.getByLabelText("Outdoor");
+    expect(checkbox).toBeInTheDocument();
+
+    await user.click(checkbox);
     expect(screen.getByTestId("filter")).toBeInTheDocument();
   });
 });
