8fd87e10d56ec25d4888fc28f234cfe2131038ec
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-11-20 13:23:13 +0100

    write test filter activities

--- tests/ActivityFilter.test.js
+++ tests/ActivityFilter.test.js
@@ -1,22 +1,34 @@
 import HomePage from "@/pages/activity/index";
-import { categories } from "@/lib/categories";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
-import { activities as activityData } from "@/lib/activities";
 
 // Test Case 2: Filter Widget Visibility
 
 describe("2.1 The widget should be hidden by default on page load", () => {
   test("Filter hidden", () => {
-    render(<HomePage activities={activityData} filter={categories} />);
+    const activities = [
+      {
+        title: "Test title",
+        categories: ["Winter"],
+      },
+    ];
+
+    render(<HomePage activities={activities} filter={[]} />);
     expect(screen.queryByText("Filter activities")).not.toBeInTheDocument();
   });
 });
 
 describe("2.2 Clicking the toggle button opens/hides the filter widget", () => {
   test("Filter toggle", async () => {
+    const activities = [
+      {
+        title: "Test title",
+        categories: ["Winter"],
+      },
+    ];
     const user = userEvent.setup();
-    render(<HomePage activities={activityData} filter={categories} />);
+
+    render(<HomePage activities={activities} filter={[]} />);
 
     const button = screen.getByRole("button", { name: /Filter/i });
     expect(button).toBeInTheDocument();
@@ -31,12 +43,20 @@ describe("2.2 Clicking the toggle button opens/hides the filter widget", () => {
 
 describe("2.3 The widget remains open if filters are applied", () => {
   test("Filter remains open", async () => {
+    const activities = [
+      {
+        title: "Test title",
+        categories: ["Winter"],
+      },
+    ];
     const user = userEvent.setup();
+    const handleFilter = jest.fn();
+
     render(
       <HomePage
-        activities={activityData}
-        filter={categories}
-        handleFilter={() => {}}
+        activities={activities}
+        filter={[]}
+        handleFilter={handleFilter}
       />
     );
 
@@ -48,7 +68,45 @@ describe("2.3 The widget remains open if filters are applied", () => {
     const checkbox = screen.getByLabelText("Outdoor");
     expect(checkbox).toBeInTheDocument();
 
-    await user.click(checkbox);
+    await user.click(checkbox); // Select filter
+
+    expect(handleFilter).toHaveBeenCalled();
     expect(screen.getByTestId("filter")).toBeInTheDocument();
+  });
+});
+
+// Test Case 3: Filter Function
+
+describe("3.1 Selecting a single filter displays only activities with that category", () => {
+  test("Display only activities with specific category", () => {
+    const activities = [
+      {
+        title: "Test title",
+        categories: ["Winter"],
+      },
+    ];
+
+    render(<HomePage activities={activities} filter={["Winter"]} />);
+
+    expect(screen.getAllByTestId("activity")).toHaveLength(activities.length);
+  });
+});
+
+describe("3.2 Selecting multiple filters displays activities that match any of the selected categories", () => {
+  test("Display only activities that match specific categories", () => {
+    const activities = [
+      {
+        title: "Test title",
+        categories: ["Winter"],
+      },
+      {
+        title: "Test title",
+        categories: ["Outdoor"],
+      },
+    ];
+
+    render(<HomePage activities={activities} filter={["Winter", "Outdoor"]} />);
+
+    expect(screen.getAllByTestId("activity")).toHaveLength(activities.length);
   });
 });
--- tests/ActivityList.test.js
+++ tests/ActivityList.test.js
@@ -1,14 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import ActivityList from "@/components/layout/ActivityList";
 import ActivityCard from "@/components/layout/ActivityCard";
-import { activities as activityData } from "@/lib/activities";
 
 // Test Case 1: Activities Rendered on Activity Page
 
 describe("1.1 When the page initially loads, verify that all activities are displayed", () => {
   test("ActivityCard rendering", () => {
-    render(<ActivityList activities={activityData} />);
-    expect(screen.getAllByTestId("activity")).toHaveLength(activityData.length);
+    const activities = [
+      {
+        title: "Test title",
+        categories: ["Winter"],
+      },
+    ];
+
+    render(<ActivityList activities={activities} />);
+    expect(screen.getAllByTestId("activity")).toHaveLength(activities.length);
   });
 });
 
